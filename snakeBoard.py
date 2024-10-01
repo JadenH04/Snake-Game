b52c3030e7465f6304607707a71edabc1c719987
MatthewCMD <matthew.x.lucius@gmail.com>
2024-09-30 20:50:56 -0400

    Got this to run on my local machine.
    Use batch file as exec

---
+++ snakeBoard.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+BACKGROUND_COLOR = "#000000"
+
+window = Tk()
+window.title("Snake game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.mainloop()
---
+++ snakeExe.bat
@@ -0,0 +1,2 @@
+snakeBoard.py
+pause
