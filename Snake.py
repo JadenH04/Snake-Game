b6be1d58b00a1efef58d3772bc4b835d2ee16d68
MatthewCMD <matthew.x.lucius@gmail.com>
2024-11-13 23:28:20 -0500

    Update Snake.py
    Fruit can now be eaten and will add to size of snake.
    Game over sequence is under construction

--- Snake.py
+++ Snake.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
@@ -23,10 +23,12 @@ class FRUIT:
         self.x = random.randint(0, cell_number - 1)
         self.y = random.randint(0, cell_number - 1)
         self.pos = pygame.math.Vector2(self.x,self.y)
+        score = 0
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y*cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+
 
 pygame.init()
 cell_size = 40
@@ -36,6 +38,16 @@ clock = pygame.time.Clock()
 
 fruit = FRUIT()
 snake = SNAKE()
+
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 70)
+    game_over_surface = my_font.render('YOU DIED\nPress Y to restart\nPress N to quit', True, (255,0,0))
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (400, 400)
+    screen.fill((0,0,0))
+    screen.blit(game_over_surface, game_over_rect)
+    pygame.display.update()
+    time.sleep(3)
 
 
 SCREEN_UPDATE = pygame.USEREVENT
@@ -47,6 +59,20 @@ while True:
             sys.exit()
         if event.type == SCREEN_UPDATE:
             snake.move_snake()
+            # Eat Fruit :)
+            if snake.body[0] == fruit.pos:
+                fruit.__init__()
+                snake.body.insert(-1, snake.body[-1])
+                fruit.score += 5
+            # Game over :(
+            if snake.body[0][0] > 19 or snake.body[0][0] < 0 or snake.body[0][1] > 19 or snake.body[0][1] < 0 or snake.body[0] == snake.body[1:]:
+                game_over()
+                if event.key == pygame.K_y:
+                    snake.__init__
+                    fruit.__init__
+                    fruit.score = 0
+                if event.key == pygame.K_n:
+                    pygame.QUIT()
         if event.type == pygame.KEYDOWN:
             if event.key ==  pygame.K_UP and snake.direction != Vector2(0, 1):
                 snake.direction = Vector2(0, -1)
@@ -56,6 +82,7 @@ while True:
                 snake.direction = Vector2(-1, 0)
             if event.key == pygame.K_RIGHT and snake.direction != Vector2(-1, 0):
                 snake.direction = Vector2(1, 0)
+            
 
     screen.fill((175, 215, 70))
     fruit.draw_fruit()
